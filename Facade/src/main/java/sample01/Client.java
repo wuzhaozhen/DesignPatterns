9cffe823f9c6cdcb907d5067aef9697fde761eaf
wuzhaozhen <2535379543@qq.com>
2018-09-25 22:22:48 +0800

    添加外观模式

---
+++ Facade/src/main/java/sample01/Client.java
@@ -0,0 +1,10 @@
+package sample01;
+
+public class Client {
+	public static void main(String args[]) {
+		GeneralSwitchFacade gsf = new GeneralSwitchFacade();
+		gsf.on();
+		System.out.println("-----------------------");
+		gsf.off();
+	}
+}
