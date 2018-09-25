9213e87e02cc31d0f6782ab2baa71e412379d6c0
wuzhaozhen <2535379543@qq.com>
2018-09-25 21:43:26 +0800

    添加原型模式

---
+++ Prototype/src/main/java/com/wuzz/simple1/Attachment.java
@@ -0,0 +1,6 @@
+package com.wuzz.simple1;
+public class Attachment {
+	public void download() {
+		System.out.println("下载附件");
+	}
+}
---
+++ Prototype/src/main/java/com/wuzz/simple2/Attachment.java
@@ -0,0 +1,9 @@
+package com.wuzz.simple2;
+
+import java.io.*;
+
+public class Attachment implements Serializable {
+	public void download() {
+		System.out.println("下载附件");
+	}
+}
