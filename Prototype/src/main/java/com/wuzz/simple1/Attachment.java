00b44f09ea3cab86892b750ee1c89f9fa88770eb
wuzhaozhen <2535379543@qq.com>
2018-09-26 11:35:37 +0800

    调整代码

--- Prototype/src/main/java/com/wuzz/simple1/Attachment.java
+++
@@ -1,6 +0,0 @@
-package com.wuzz.simple1;
-public class Attachment {
-	public void download() {
-		System.out.println("下载附件");
-	}
-}
--- Prototype/src/main/java/com/wuzz/simple2/Attachment.java
+++
@@ -1,9 +0,0 @@
-package com.wuzz.simple2;
-
-import java.io.*;
-
-public class Attachment implements Serializable {
-	public void download() {
-		System.out.println("下载附件");
-	}
-}
