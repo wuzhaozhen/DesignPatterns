26afdf5bf35af3213cbf8845387ae84c3989fdcc
wuzhaozhen <2535379543@qq.com>
2018-09-26 11:06:08 +0800

    调整建造者模式

---
+++ 4-Builder/src/main/java/example1/KFCWaiter.java
@@ -0,0 +1,15 @@
+package example1;
+
+public class KFCWaiter {
+	private MealBuilder mb;
+
+	public void setMealBuilder(MealBuilder mb) {
+		this.mb = mb;
+	}
+
+	public Meal construct() {
+		mb.buildFood();
+		mb.buildDrink();
+		return mb.getMeal();
+	}
+}
