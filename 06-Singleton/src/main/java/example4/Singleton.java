8b325b94977c2991a82bde4fe63a7e27c7c98caf
wuzhaozhen <2535379543@qq.com>
2018-09-29 17:34:55 +0800

    添加文件

--- 06-Singleton/src/main/java/example4/Singleton.java
+++ 06-Singleton/src/main/java/example4/Singleton.java
@@ -1,14 +1,14 @@
-package example3;
+package example4;
 
 /**
- * 懒汉模式
+ * 饿汉模式
  * 
  * @author wuzz
  * @date 2018年9月29日 下午5:15:25
  */
 public class Singleton {
 	// 创建类的唯一实例
-	private static Singleton instance = null;
+	private static Singleton instance = new Singleton();
 
 	// 构造方法私有化
 	private Singleton() {
@@ -16,8 +16,6 @@ public class Singleton {
 
 	// 提供获取实例的公用方法
 	public static Singleton getInstance() {
-		if (instance == null)
-			instance = new Singleton();
 		return instance;
 	}
 }
