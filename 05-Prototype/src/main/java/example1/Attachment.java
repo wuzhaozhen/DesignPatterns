63b647e9d47ac6beebba14d81ec79e2b27479bb4
wuzhaozhen <2535379543@qq.com>
2018-10-10 14:48:24 +0800

    调整创建型模式代码

--- 05-Prototype/src/main/java/example1/Attachment.java
+++ 05-Prototype/src/main/java/example1/Attachment.java
@@ -1,7 +1,7 @@
 package example1;
 
 public class Attachment {
-	public void download() {
-		System.out.println("下载附件");
+	public void download(Email email) {
+		System.out.println(email.getName() + "今年" + email.getAge() + "岁,下载附件");
 	}
 }
--- 05-Prototype/src/main/java/example1/Client.java
+++ 05-Prototype/src/main/java/example1/Client.java
@@ -1,17 +1,37 @@
 package example1;
 
+/**
+ * 浅克隆
+ * 
+ * @author wuzz
+ * @date 2018年10月10日 下午2:16:27
+ */
 public class Client {
 	public static void main(String a[]) {
 		Email email, copyEmail;
 
-		email = new Email();
+		email = new Email("张三", 12);
 
 		copyEmail = (Email) email.clone();
-
+		
 		System.out.println("email==copyEmail?");
 		System.out.println(email == copyEmail);
-
+		
+		System.out.println(email.hashCode());
+		System.out.println(copyEmail.hashCode());
+		
+		
 		System.out.println("email.getAttachment==copyEmail.getAttachment?");
 		System.out.println(email.getAttachment() == copyEmail.getAttachment());
+		
+		
+		Attachment emailAttachment = email.getAttachment();
+		Attachment copyEmailAttachment = copyEmail.getAttachment();
+		System.out.println(emailAttachment.hashCode());
+		System.out.println(copyEmailAttachment.hashCode());
+		
+		copyEmail.setAge(13);
+		emailAttachment.download(email);
+		copyEmailAttachment.download(copyEmail);
 	}
 }
